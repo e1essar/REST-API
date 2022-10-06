51911b30bda407239034c3f7ddd539e28ec5d2b6
e1essar <igorvaliulin770@gmail.com>
2022-10-07 00:28:55 +0700

    feat: create db and controlller

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express')
-
+const userRouter = require('./routes/user.routes')
 const PORT = process.env.PORT || 8080
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('HELLO POSTGRES + NODEJS1')
-})
+app.use('/api', userRouter)
 
 app.listen(PORT, () => console.log('server started on ' + PORT))

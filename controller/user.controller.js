0278575945843e027a5e964fa452bd26f4c09406
e1essar <igorvaliulin770@gmail.com>
2022-10-07 02:42:51 +0700

    fix: fix2

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,6 +1,10 @@
+const db = require('../db')
 class UserController {
     async createUser(req,res) {
+        const {name,surname} = req.body
+        const newPerson = await db.query('INSERT INTO person (name,surname) values ($1, $2) RETURNING *', [name,surname])
 
+        res.json(newPerson)
     }
     async getUsers(req,res) {
 
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const userRouter = require('./routes/user.routes')
 const PORT = process.env.PORT || 8080
 
 const app = express()
-
+app.use(express.json())
 app.use('/api', userRouter)
 
 app.listen(PORT, () => console.log('server started on ' + PORT))
